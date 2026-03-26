f6c998e1b6495b3bed0fd20180360c61dda41157
Moon ChangWuk <moonchang0193@gmail.com>
2026-03-26 20:47:43 +0900

    Feat: Monster 클래스 구현

--- TeamProject13/Monster.h
+++ TeamProject13/Monster.h
@@ -4,5 +4,13 @@
 #include <string>
 
 class Monster {
-
+private:
+	std::string name;
+	int pressure;
+public:
+	Monster(std::string name, int pressure) { this->name = name; this->pressure = pressure; }
+	~Monster() {}
+	std::string getName() { return name; }
+	int getHP() { return pressure; }
+	void takeDamage(int damage) { pressure -= damage; }
 };
