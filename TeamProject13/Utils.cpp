299b523e65aa0c7c64ed44ce1722fc1d7321a04b
DESKTOP-F6FGIDC\TaitoP <taito.p23@gmail.com>
2026-03-26 16:12:19 +0900

    feat: 공용 유틸리티(Utils) 클래스 구현 및 난수 생성기 추가 (수정)

--- TeamProject13/Utils.cpp
+++ TeamProject13/Utils.cpp
@@ -1,1 +1,18 @@
+// Utils.cpp 구분선/ 랜덤 함수
+
 #include "Utils.h"
+#include <iostream>
+#include <random>
+
+void Utils::PrintLine(char ch, int len)
+{
+    for (int i = 0; i < len; ++i) std::cout << ch;
+    std::cout << "\n";
+}
+
+int Utils::GetRandom(int min, int max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(min, max);
+    return dis(gen);
+}
--- TeamProject13/Utils.h
+++ TeamProject13/Utils.h
@@ -1,5 +1,17 @@
+// Utils.h
+// 담당: 공용 - 수정 금지
+// 역할: 랜덤, 콘솔 출력 관련 공용 도구 모음
+
 #pragma once
+#include <string>
+
 class Utils
 {
+public:
+	// 구분선 출력 ex) Utils::PrintLine('=', 42)
+	static void PrintLine(char ch = '-', int len = 42);
+
+	// 난수 생성 함수 추가 ex) Utils::GetRandom(1, 100)
+	static int GetRandom(int min, int max);
 };
 
