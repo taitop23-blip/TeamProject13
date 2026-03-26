6682860ae4b3698c3b8b0409dc0797e3cbe3c113
DESKTOP-F6FGIDC\TaitoP <taito.p23@gmail.com>
2026-03-26 15:59:16 +0900

    feat: 공용 유틸리티(Utils) 클래스 구현 및 난수 생성기 추가
    PrintLine 함수 추가
    GetRandom 난수 생성 함수 추가

--- TeamProject13/Utils.h
+++ TeamProject13/Utils.h
@@ -1,5 +1,14 @@
+// Utils.h
+// 담당: 공용 - 수정 금지
+// 역할: 랜덤, 콘솔 출력 관련 공용 도구 모음
+
 #pragma once
+#include <string>
+
 class Utils
 {
+public:
+	// 구분선 출력 ex) Utils::PrintLine('=', 42)
+	static void PrintLine(char ch = '-', int len = 42);
 };
 
