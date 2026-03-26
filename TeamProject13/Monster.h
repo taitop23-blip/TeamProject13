1fa94a9aedfd241a4fa55c6b090be782c7ad0fc4
Moon ChangWuk <moonchang0193@gmail.com>
2026-03-26 21:00:25 +0900

    Refac: Monster 수정

--- TeamProject13/Monster.h
+++ TeamProject13/Monster.h
@@ -7,10 +7,17 @@ class Monster {
 private:
 	std::string name;
 	int pressure;
+	int maxPressure;
+	int atk;
+	int def;
 public:
-	Monster(std::string name, int pressure) { this->name = name; this->pressure = pressure; }
+	Monster(std::string name, int pressure, int atk, int def) { this->name = name; this->pressure = pressure; maxPressure = pressure; 
+																this->atk = atk; this->def = def; }
 	~Monster() {}
-	std::string getName() { return name; }
-	int getHP() { return pressure; }
+	std::string getName() const{ return name; }
+	int getPressure() const{ return pressure; }
+	int getMaxPressure() const{ return maxPressure; }
+	int getAtk() const{ return atk; }
+	int getDef() const{ return def; }
 	void takeDamage(int damage) { pressure -= damage; }
 };
