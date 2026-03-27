934d8540e5ce7d8fab160c382ac923f50d0d3f98
SEOHEE\dbstj <dbstjgml000>
2026-03-27 13:16:46 +0900

    윤서희 : player.h player.cpp 작성 완료

--- TeamProject13/Player.cpp
+++ TeamProject13/Player.cpp
@@ -1,1 +1,66 @@
 #include "Player.h"
+
+// add함수
+void Player::AddMental(int val)
+{
+	mental += val;
+	if (mental > maxMental)
+	{
+		mental = maxMental;
+	}
+}
+
+void Player::AddFocus(int val)
+{
+	focus += val;
+	if (focus > maxFocus)
+	{
+		focus = maxFocus;
+	}
+}
+
+void Player::AddProgress(int val)
+{
+	progress += val;
+	if (progress > 100)
+	{
+		progress = 100;
+	}
+}
+
+//sub 함수
+void Player::SubMental(int val)
+{
+	mental -= val;
+	if (mental < 0)
+	{
+		mental = 0;
+	}
+}
+
+
+void Player::SubFocus(int val)
+{
+	focus -= val;
+	if (focus < 0)
+	{
+		focus = 0;
+	}
+}
+
+// 생성자
+Player::Player(int mental, int focus, int atk, int def)
+{
+	this->mental = mental;
+	this->maxMental = mental;
+
+	this->focus = focus;
+	this->maxFocus = focus;
+
+	this->atk = atk;
+	this->def = def;
+}
+
+
+
+
--- TeamProject13/Player.h
+++ TeamProject13/Player.h
@@ -5,6 +5,7 @@
 
 class Player {
 private:
+
 	int Level = 1;
 	int exp = 0;
 	int maxExp = 100;
@@ -15,4 +16,36 @@ private:
 	int def;                 // 방어력
 	int progress = 0;        // 진행도 (0~100%)
 	int life = 3;            // 목숨
+
+public:
+
+	// get 함수
+	int GetLevel() const { return Level; }
+	int GetExp() const { return exp; }
+	int GetMental() const { return mental; }
+	int GetMaxMental() const { return maxMental; }
+	int GetFocus() const { return focus; }
+	int GetMaxFocus() const { return maxFocus; }
+	int GetProgress() const { return progress; }
+	int GetLife() const { return life; }
+	int GetAtk() const { return atk; }
+	int GetDef() const { return def; }
+
+	//add 함수
+	void AddMental(int val);
+	void AddFocus(int val);
+	void AddProgress(int val);
+	
+	//sub 함수
+	void SubMental(int val);
+	void SubFocus(int val);
+	void SubLife() { life -= 1; }
+
+	//전투 관련 로직
+	int getHP() const { return mental; } // Battlemanager 작업용
+	void takeDamage(int damage) { SubMental(damage); }
+	bool IsDead() const {return life<= 0; }
+
+	// 생성자
+	Player(int mental, int focus, int atk, int def);
 };
