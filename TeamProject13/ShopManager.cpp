8a7c14747fce0398524dcde83df970175a19b8e5
Moon ChangWuk <moonchang0193@gmail.com>
2026-03-27 17:12:55 +0900

    Feat: 상점 대략적 구현

--- TeamProject13/ShopManager.cpp
+++ TeamProject13/ShopManager.cpp
@@ -1,1 +1,53 @@
-#include "ShopManager.h"
+//#include <iostream>
+//#include "ShopManager.h"
+//#include "Utils.h"
+//void ShopManager::displayItems() {
+//	Utils::PrintLine('=');
+//	std::cout << "여어~ 요즘 일은 어때?" << std::endl << std::endl;
+//
+//	std::vector<Item>::iterator it;
+//	int index = 1;
+//	for (it = shopInventory.begin(); it != shopInventory.end(); ++it) {	// 상품 목록 출력 
+//		std::cout << index << ": " << it->getName() << "  ";
+//		std::cout << "가격: " << it->getPrice() << "원" << std::endl;
+//		++index;
+//	}
+//}
+//void ShopManager::buyItem(Player& player) {
+//	int select;
+//	std::cout << "구매할 상품 번호를 입력: ";
+//	std::cin >> select;
+//	select -= 1; // 선택한 물품의 인덱스와 벡터의 해당 물건 인덱스 동기화
+//
+//	int itemPrice = shopInventory[select].getPrice();
+//	std::string itemName = shopInventory[select].getName();
+//	if (player.getGold() < itemPrice) {		// 소지금이 물품가액보다 작으면 종료 
+//		std::cout << "돈이 모자른것 같은데?" << std::endl << std::endl;
+//		return;
+//	}
+//	player.subGold(itemPrice);
+//	PlayerInventory.pushItem(itemName, itemPrice); // 플레이어 인벤토리 클래스 내 인벤토리 벡터에 item 임시 객체 push
+//
+//	std::cout << "매번 고마워. 쉬엄쉬엄하라구." << std::endl << std::endl;
+//	return;
+//}
+//void ShopManager::shopConsole(Player& player) {
+//	displayItems();
+//
+//	while (1) {
+//		int select;
+//		std::cout << "1) 물건을 산다. 2) 떠난다" << std::endl;
+//		std::cout << "번호 선택: ";
+//		std::cin >> select;
+//
+//		switch (select) {
+//		case 1:
+//			buyItem(player);
+//			break;
+//		case 2:
+//			std::cout << "다음에 또 오라구!" << std::endl;
+//			return;
+//		}
+//	}
+//
+//}
--- TeamProject13/ShopManager.h
+++ TeamProject13/ShopManager.h
@@ -1,5 +1,19 @@
 #pragma once
-class ShopManager
-{
+#include "Player.h"
+#include "Item.h"
+#include <vector>
+
+class Item;
+
+class ShopManager {
+private:
+	std::vector<Item> shopInventory; // Item class vector. price, itemName 정보 필요 
+public:
+	ShopManager() {}	// Item class의 instance 생성 시, shopInventory 채울 예정 
+	~ShopManager() {}
+	void displayItems();     	// 상점 물품 출력
+	void buyItem(Player& player);	// 구매, 플레이어 인벤토리에 아이템 추가
+//	void sellItem(Player& player);	// 판매, 아직 구현 미정, 구현 시 플레이어 인벤토리 아이템 소멸  
+	void shopConsole(Player& player);			// 상점 조작 콘솔 
 };
 
