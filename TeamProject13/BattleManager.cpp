efc4e4e91e8675e2a86e1f0833dd2a7665888b7a
DESKTOP-P5R9FA6\user <ljh347755@gmail.com>
2026-03-27 11:48:28 +0900

    BattleManager(PlayerTurn, MonsterTurn, IsBattleOver) 임시 세팅 완료 및 헤더에 도망 처리 부분 추가

--- TeamProject13/BattleManager.cpp
+++ TeamProject13/BattleManager.cpp
@@ -29,20 +29,37 @@ void BattleManager::StartBattle(Player& player, Monster& monster)
 
 	if (player.getHP() <= 0)
 		ProcessDefeat(player);
-	else if (monster.getHP() <= 0)
+	else if (monster.getPressure() <= 0)
 		ProcessVictory(player, monster);
+	else if (isRunningAway)
+		ProcessRunAway();
 }
 
 void BattleManager::DisplayStatus(const Player& player, const Monster& monster)
 {
 	std::cout << "===============================\n";
 	std::cout << "[플레이어]\n";
-	std::cout << std::left << std::setw(10) << "멘탈" << ": " << std::right << std::setw(3) << "???" << "/" << std::setw(3) << "???" << '\n';
-	std::cout << std::left << std::setw(10) << "집중력" << ": " << std::right << std::setw(3) << "???" << "/" << std::setw(3) << "???" << '\n';
+	std::cout << std::left << std::setw(10) << "멘탈"
+		<< " : " << std::right << std::setw(3) << player.GetMental()
+		<< "/" << std::setw(3) << player.GetMaxMental() << '\n';
+
+	std::cout << std::left << std::setw(10) << "집중력"
+		<< " : " << std::right << std::setw(3) << player.GetFocus()
+		<< "/" << std::setw(3) << player.GetMaxFocus() << '\n';
+
+	int life = player.GetLife();
+
+	std::cout << std::left << std::setw(10) << "뽀삐사진" << " : ";
+		for (int i = 0; i < life; i++)
+		{
+			std::cout << "♥";
+		}
+	std::cout << '\n';
 
 	std::cout << "[빌런]\n";
-	std::cout << std::left << std::setw(10) << "압박감" << ": " << std::right << std::setw(3) << "???" << "/" << std::setw(3) << "???" << '\n';
-	std::cout << std::left << std::setw(10) << "위험도" << ": " << std::right << std::setw(3) << "???" << "/" << std::setw(3) << "???" << '\n';
+	std::cout << std::left << std::setw(10) << "압박감"
+		<< " : " << std::right << std::setw(3) << monster.getPressure()
+		<< "/" << std::setw(3) << monster.getMaxPressure() << '\n';
 
 	std::cout << "===============================\n";
 }
@@ -66,9 +83,21 @@ void BattleManager::PlayerTurn(Player& player, Monster& monster)
 	switch (choice)
 	{
 		case 1:
-			std::cout << "여기 정리 자료 있습니다.!\n";
-			std::cout << "빌런 이름" << "????" << "의 데미지를 주었습니다.\n";
+		{
+			int damage = player.GetAtk() - monster.getDef();
+
+			if (damage < 0)
+				damage = 0;
+
+			monster.takeDamage(damage);
+
+			std::cout << "여기 정리 자료 있습니다!\n";
+			std::cout << monster.getName() << "에게 압박감이 " << damage << "만큼 감소했습니다.\n";
+
+			std::cout << monster.getName() << "의 현재 압박감 : " << monster.getPressure() << " / " << monster.getMaxPressure() << '\n';
 			break;
+		}
+			
 
 		case 2:
 			if (skillManager)
@@ -82,6 +111,7 @@ void BattleManager::PlayerTurn(Player& player, Monster& monster)
 			break;
 
 		case 4:
+		{
 			std::cout << "도망을 시전합니다.....\n";
 
 			int chance = rand() % 100;
@@ -93,12 +123,42 @@ void BattleManager::PlayerTurn(Player& player, Monster& monster)
 			}
 			else
 			{
-				std::cout << "빌런 이름 : 어디가나 당장 이리와!\n";
+				std::cout << monster.getName() << " : 어디가나 당장 이리와!\n";
 			}
 			break;
-
+		}
 		default:
 			std::cout << "선택 방식이 잘못되었습니다.. 다시 입력해주세요.\n";
 			break;
 	}
+}
+
+void BattleManager::MonsterTurn(Player& player, Monster& monster)
+{
+	std::cout << "\n[ 몬스터 턴 ]\n";
+	std::cout << monster.getName() << "이(가) " << player.getName << "를 압박합니다.\n";
+
+	int damage = monster.getAtk() - player.GetDef();
+
+	if (damage < 0)
+		damage = 0;
+
+	player.takeDamage(damage);
+
+	std::cout << damage << "의 만큼 멘탈이 피해를 받았습니다!!\n";
+	std::cout << "현재 멘탈 : " << player.GetMental() << " / " << player.GetMaxMental() << "\n";
+}
+
+bool BattleManager::IsBattleOver(const Player& player, const Monster& monster)
+{
+	if (monster.getPressure() <= 0)
+		return true;
+
+	if (player.getHP <= 0)
+		return true;
+
+	if (isRunningAway)
+		return true;
+
+	return false;
 }
