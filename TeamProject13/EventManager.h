c8d80e326d80099471d9c4e70b9830359d0f966a
검성\twili <jyjh4837@gmail.com>
2026-03-27 12:20:51 +0900

    이벤트 매니저 헤더파일

--- TeamProject13/EventManager.h
+++ TeamProject13/EventManager.h
@@ -1,5 +1,13 @@
 #pragma once
-class EventManeger
+#include <string>
+#include "Player.h"
+
+class EventManeger //이벤트 매니저 클래스
 {
+public:
+	void::EventManeger; void ShowBonusMenu(Player& p);
+
+private:
+	void::EventManeger; void ApplyBonus(Player& p, int choice);
 };
 
