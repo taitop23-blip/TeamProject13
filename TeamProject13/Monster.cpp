2519b7bb4ed04e55a4d07779b073872bb3d0d9ec
Moon ChangWuk <moonchang0193@gmail.com>
2026-03-27 20:28:15 +0900

    Feat: Monster 클래스 함수 추가 및 수정

--- TeamProject13/Monster.cpp
+++ TeamProject13/Monster.cpp
@@ -1,5 +1,18 @@
 #include "Monster.h"
 
-Monster DepartmentManager("ºÎÀå´Ô", 50, 20, 30);
-Monster Designer("±âÈ¹ÀÚ", 20, 50, 20);
-Monster Boss("ÆÀÀå´Ô", 100, 50, 50);
+void Monster::TakeDamage(int damage) {
+	if (damage >= pressure) {
+		pressure = 0;
+		defeated = true;
+		return;
+	}
+	pressure -= damage;
+}
+void Monster::MonsterAction(Player& player) { 
+	int playerDef = player.GetDef();
+	int damage = atk - playerDef;
+	player.SubMental(damage);
+}
+Monster DepartmentManager("ºÎÀå´Ô", 50, 20, 30, 50, 500);
+Monster Designer("±âÈ¹ÀÚ", 20, 50, 20, 50, 500);
+Monster Boss("ÆÀÀå´Ô", 100, 50, 50, 100, 1000);
