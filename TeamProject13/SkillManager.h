31871392663ed8915862009a2dafabc1d5c041fc
검성\twili <jyjh4837@gmail.com>
2026-03-27 11:36:47 +0900

    std::로 변경

--- TeamProject13/SkillManager.h
+++ TeamProject13/SkillManager.h
@@ -5,8 +5,6 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 enum class SkillType  //스킬타입 선언
 {
 	ATTACK, DEFENSE, HEAL
@@ -15,7 +13,7 @@ enum class SkillType  //스킬타입 선언
 class SkillManager //Skill 클래스
 {
 public:
-	string name;
+	std::string name;
 	SkillType type;
 
 	int power;
@@ -23,7 +21,7 @@ public:
 	int cooltime;
 	int currentCoolTime;
 
-	void Skill(string n, SkillType t, int p, int cost, int ct); //스킬 생성자
+	void Skill(std::string n, SkillType t, int p, int cost, int ct); //스킬 생성자
 
 	bool CanUse(int currentfocus); //스킬 사용가능여부 확인
 	void Use(Player& user, Player& target); //스킬 실행 함수
